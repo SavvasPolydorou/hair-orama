7d68d26510afa76b39cae6293c3db6ebe3837f63
savvaspolydorou <savvas330@hotmail.com>
2024-06-16 10:58:32 +0300

    working version

--- apps/hair-orama/src/app/app.config.ts
+++ apps/hair-orama/src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app-routing.module';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideClientHydration(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)],
+  providers: [provideClientHydration(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(withFetch())],
 };
